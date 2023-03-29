d46f260fd004b175d031d1e53d43a94ba7535ee9
Alex <romanov123@yandex.ru>
2023-03-29 19:43:27 +0300

    zad4

---
+++ zad 4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("a:");
+int a = int.Parse (Console.ReadLine());
+Console.WriteLine("b:");
+int b = int.Parse (Console.ReadLine());
+Console.WriteLine("c:");
+int c = int.Parse (Console.ReadLine());
+
+int max = a;
+
+if ( b > max )
+{
+   max = b;
+}
+if ( c > max )
+{
+   max = c;
+}
+Console.WriteLine ($"max = {max}");
